2349177440bc4e40d228589b1aebaf0ea84ea5d3
eduardops <eduardops@gmail.com>
2018-09-06 11:34:59 -0300

    adiciona contador com react redux e botoes que incrementam e decrementam o valor

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
+import {increment, decrement, increment_amount, decrement_amount} from '../redux/index'
+
 class Counter extends Component {
   render() {
     return (
       <div>
-        <h1>1</h1>
+        <h1>{this.props.counter}</h1>
+            <button onClick={() => this.props.decrement()}>Decrement</button>
+            <button onClick={() => this.props.increment()}>Increment</button>
+            <button onClick={() => this.props.decrement_amount(this.props.counter)}>Decrement {this.props.counter}</button>
+            <button onClick={() => this.props.increment_amount(this.props.counter)}>Increment {this.props.counter}</button>
       </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  
+    counter: state.counter
 });
 
 const mapDispatchToProps = {
-  
+    increment,
+    decrement,
+    increment_amount,
+    decrement_amount
 };
 
 export default connect(
